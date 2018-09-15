c1a2dfb0b57913c67390848b7c756e702f6ea825
ghof <ghof+sfu_connect@sfu.ca>
2018-09-15 17:09:52 -0400

    adds podfile and fake data for chart

---
+++ better-belly/better-belly/BarChartViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BarChartViewController.swift
+//  better-belly
+//
+//  Created by Grazietta Hof on 2018-09-15.
+//  Copyright © 2018 Ansar Khan. All rights reserved.
+//
+
+import UIKit
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
