5d86ad8f0438c99b45f0b1b5d5ae26f78a5bc70c
Unknown <ansark@hotmail.com>
2018-09-15 09:48:34 -0400

    Initialize iOS App

---
+++ better-belly/better-belly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  better-belly
+//
+//  Created by Ansar Khan on 2018-09-15.
+//  Copyright © 2018 Ansar Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
