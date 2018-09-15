10cc838bf899d01f236496399f8f056d6342d97e
ghof <ghof+sfu_connect@sfu.ca>
2018-09-15 12:44:31 -0400

    adds navigation view controller and page view controller for charts

---
+++ better-belly/better-belly/PageViewController.swift
@@ -0,0 +1,75 @@
+//
+//  PageViewController.swift
+//  better-belly
+//
+//  Created by Grazietta Hof on 2018-09-15.
+//  Copyright © 2018 Ansar Khan. All rights reserved.
+//
+
+import UIKit
+
+//page view controller copied from https://spin.atomicobject.com/2015/12/23/swift-uipageviewcontroller-tutorial/
+
+class PageViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newChartViewController("BarChart"),
+                self.newChartViewController("LineChart"),
+                self.newChartViewController("Feedback")]
+    }()
+    
+    private func newChartViewController(_ identifier: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(identifier)ViewController")
+    }
+}
+
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
